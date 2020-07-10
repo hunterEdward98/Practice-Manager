c7e8af191d22a71e2d5e1538a227e65a2770673e
Hunter Edward <Hunter.scheel@outlook.com>
2020-07-09 22:16:01 -0500

    router changes

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -17,13 +17,13 @@ class Header extends React.Component {
                     </div>
                     {this.props.user.auth_level ?
                         <div className='col-3 col-md-2'><button className='col-12 btn signout' onClick={() => { this.props.dispatch({ type: 'LOGOUT' }); this.setState({ loggedIn: false }) }}>Sign Out</button> </div> :
-                        <Link exact to='/sign-in' className='col-3 col-md-2'><button className='col-12 btn signin' onClick={() => this.setState({ loggedIn: true })}>Sign In</button></Link>}
+                        <Link to='/sign-in' className='col-3 col-md-2'><button className='col-12 btn signin' onClick={() => this.setState({ loggedIn: true })}>Sign In</button></Link>}
                     {this.props.user.auth_level >= 3 ?
                         <nav className='col-12'>
-                            <NavLink exact to='/#/' className='btn blk col-12 col-md-3'>Home</NavLink>
-                            <NavLink exact to='/#/set-manager' className='btn blk col-12 col-md-3'>Test Set Manager</NavLink>
-                            <NavLink exact to='/#/swimmer-search' className='btn blk col-12 col-md-3'>Swimmers</NavLink >
-                            <NavLink exact to='/#/super-admin' className='btn blk col-12 col-md-3'>Admin Tools</NavLink></nav> :
+                            <NavLink to='/#/' className='btn blk col-12 col-md-3'>Home</NavLink>
+                            <NavLink to='/#/set-manager' className='btn blk col-12 col-md-3'>Test Set Manager</NavLink>
+                            <NavLink to='/#/swimmer-search' className='btn blk col-12 col-md-3'>Swimmers</NavLink >
+                            <NavLink to='/#/super-admin' className='btn blk col-12 col-md-3'>Admin Tools</NavLink></nav> :
                         <nav className='col-12'>
                             <NavLink to='/#/' className='btn blk col-12 col-sm-4'>Home</NavLink>
                             <NavLink to='/#/set-manager' className='btn col-12 blk col-sm-4'>Test Set Manager</NavLink>
