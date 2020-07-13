6722446121642083c35b6a224bae23483c41862e
Hunter Edward <Hunter.scheel@outlook.com>
2020-07-13 14:02:58 -0500

    more commenting, sign in, Users, and AdminTools. also some minor refactoring

--- src/Components/AdminTools/AdminTools.js
+++ src/Components/AdminTools/AdminTools.js
@@ -5,19 +5,18 @@ import Users from './Users/Users'
 import AddSwimmerForm from './AddSwimmerForm/AddSwimmerForm'
 import AddEvent from './AddEvent/AddEvent'
 class AdminTools extends React.Component {
-    componentDidMount() {
-        this.props.dispatch({ type: 'FETCH_ALL_USERS' })
-    }
     render() {
         return (
             <div className='container my-5'>
                 <div className='row justify-content-center'>
                     <div className='col-12 col-md-8'>
+                        {/* this page has 3 sub-routes. we need a navbar to suggest that. */}
                         <NavLink exact to='/super-admin/users' className='col-12 col-sm-4 h2 btn adminNav'>Users</NavLink>
                         <NavLink exact to='/super-admin/swimmers' className='col-12 col-sm-4 h2 btn adminNav'>Add Swimmers</NavLink>
                         <NavLink exact to='/super-admin/sets' className='col-12 col-sm-4 h2 btn adminNav'>Add Sets</NavLink>
                     </div>
                 </div>
+                {/* this page needs 3 sub-routes. one for user edits, one for swimmers, and one for events*/}
                 <Route path='/super-admin/users'><Users /></Route>
                 <Route path='/super-admin/swimmers'><AddSwimmerForm /></Route>
                 <Route path='/super-admin/sets'><AddEvent /></Route>
@@ -25,9 +24,11 @@ class AdminTools extends React.Component {
         )
     }
 }
+//get all users from redux
 const mapStateToProps = (state) => {
     return {
         users: state.users
     }
 }
+//connect to redux, get props
 export default connect(mapStateToProps)(AdminTools)
--- src/Components/AdminTools/Users/Users.js
+++ src/Components/AdminTools/Users/Users.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 import User from './UserItem/User'
 class AdminTools extends React.Component {
+    //when the component mounts, get all users from the database.
     componentDidMount() {
         this.props.dispatch({ type: 'FETCH_ALL_USERS' })
     }
@@ -9,28 +10,33 @@ class AdminTools extends React.Component {
         return (
             <div className='container my-5'>
                 <h2>
+                    {/* tell the user what their authorization level is */}
                     {this.props.user.auth_level == 3 && <h3> YOU ARE SIGNED IN AS A: Coach</h3>}
                     {this.props.user.auth_level == 4 && <h3> YOU ARE SIGNED IN AS A: Head Coach</h3>}
                     {this.props.user.auth_level == 5 && <h3> YOU ARE SIGNED IN AS AN: Administrator</h3>}
+                    {this.props.user.auth_level == 6 && <h3> YOU ARE SIGNED IN AS THE: Site Owner</h3>}
                 </h2>
+                {/* display a table. */}
                 <table className='table table-dark mb-5'>
                     <thead><tr><th className='text-center'>User Name</th>
                         <th className='text-center'>Authorization</th>
                         <th className='text-center'>Edit</th>
                         <th className='text-center'>Delete</th>
                     </tr></thead>
+                    {/* if there are are any users in the global state, map them into the table as User objects */}
                     {this.props.users[0] &&
                         <tbody>{this.props.users.map(x => <User data={x} />)}</tbody>
                     }
-                    {console.log(this.props.users)}
                 </table></div>
         )
     }
 }
+//get user and all users from redux
 const mapStateToProps = (state) => {
     return {
         user: state.user,
         users: state.users
     }
 }
+// connect to redux, get props
 export default connect(mapStateToProps)(AdminTools)
