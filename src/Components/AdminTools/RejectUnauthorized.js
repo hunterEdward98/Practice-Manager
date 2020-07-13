f7a655daedecebb008c5edf511f99f24d2ddb0c7
Hunter Edward <Hunter.scheel@outlook.com>
2020-07-13 11:18:50 -0500

    styling... I think

--- src/Components/AdminTools/RejectUnauthorized.js
+++ src/Components/AdminTools/RejectUnauthorized.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import {NavLink} from 'react-router-dom'
+import {Link} from 'react-router-dom'
 const AdminTools = () => {
-    return (<div className = 'h1'>You Don't have access to this page. Please <NavLink exact to='/sign-in' className='btn btn-info'>Sign In</NavLink> </div>)
+    return (<div className = 'h1'>You Don't have access to this page. Please <Link exact to='/sign-in' className='btn btn-info'>Sign In</Link> </div>)
 }
 export default connect()(AdminTools)
