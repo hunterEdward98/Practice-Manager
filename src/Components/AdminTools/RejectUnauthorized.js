9d0e7ccf0b3054e48eb9e1ccbeed3ad20e42c9dd
Hunter Edward <Hunter.scheel@outlook.com>
2020-07-13 13:45:52 -0500

    more commenting, rejection component and header

--- src/Components/AdminTools/RejectUnauthorized.js
+++ src/Components/AdminTools/RejectUnauthorized.js
@@ -2,11 +2,14 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 class AdminTools extends React.Component {
+
     render() {
         return (<div className='h1' > You Don't have access to this page.
             { this.props.user.name ?
+                // if they are signed in, but have an authorization level of 0, tell them to ask for approval
                 <> Please ask a coach or administrator to approve your request to join the organization</>
                 :
+                //if they aren't signed in, tell them to go to the sign in page
                 <>
                     Please
                     <Link exact to='/sign-in' className='btn btn-info'>
@@ -17,9 +20,13 @@ class AdminTools extends React.Component {
 
     }
 }
+
+//get user from redux
 const mapStateToProps = (state) => {
     return {
         user: state.user
     }
 }
+
+//connect to redux, get some props
 export default connect(mapStateToProps)(AdminTools)
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,27 +2,27 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { NavLink, Link } from 'react-router-dom'
 class Header extends React.Component {
-    state = { loggedIn: false }
-    componentWillReceiveProps() {
-        this.setState({
-            user: this.props.user
-        })
-    }
+    //by default, the user will be assumed to not be logged in
     render() {
         return (<div>
             <div className='container'>
                 <div className='row'>
                     <div className='h1 col-9 col-md-10 text-left'>
+                        {/* app title */}
                         Practice Manager
                     </div>
                     {this.props.user.name ?
-                        <div className='col-3 col-md-2'><button className='col-12 btn signout' onClick={() => { this.props.dispatch({ type: 'LOGOUT' }); this.setState({ loggedIn: false }) }}>Sign Out</button> </div> :
-                        <Link to='/sign-in' className='col-3 col-md-2'><button className='col-12 btn signin' onClick={() => this.setState({ loggedIn: true })}>Sign In</button></Link>}
+                        // if the user is logged in, display a log out button
+                        <div className='col-3 col-md-2'><button className='col-12 btn signout' onClick={() => { this.props.dispatch({ type: 'LOGOUT' }); }}>Sign Out</button> </div> :
+                        //if the user is not logged in, display a log in button
+                        <Link to='/sign-in' className='col-3 col-md-2'><button className='col-12 btn signin' >Sign In</button></Link>}
                     {this.props.user.auth_level >= 3 ?
+                        //only display the admin page if the user has an auth_level of 3 or above
                         <nav className='col-12'>
                             <NavLink to='/set-manager' className=' blk col-12 tab col-sm-4'>Test Set Manager</NavLink>
                             <NavLink to='/swimmer-search' className=' blk col-12 tab col-sm-4'>Swimmers</NavLink >
                             <NavLink to='/super-admin' className=' blk col-12 tab col-sm-4'>Admin Tools</NavLink></nav> :
+                        //otherwise display these 2. the reason we need to split this is because the tab size depends on how many there are
                         <nav className='col-12'>
                             <NavLink to='/set-manager' className=' col-12 blk tab col-sm-6'>Test Set Manager</NavLink>
                             <NavLink to='/swimmer-search' className=' blk col-12 tab col-sm-6'>Swimmers</NavLink ></nav>}
@@ -32,9 +32,11 @@ class Header extends React.Component {
         )
     }
 }
+// get user from redux
 const mapStateToProps = (state) => {
     return {
         user: state.user
     }
 }
+//connect to redux, get props
 export default connect(mapStateToProps)(Header)
